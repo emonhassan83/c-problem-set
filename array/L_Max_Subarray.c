b31f66ed014baed76deee313f27c05ab36d49690
Emon <emonhasan7650@gmail.com>
2024-05-16 14:39:05 +0600

    some array problem inilaize

---
+++ array/L_Max_Subarray.c
@@ -0,0 +1,9 @@
+/*
+* url: https://codeforces.com/group/MWSDmqGsZm/contest/219774/problem/L */
+
+#include <stdio.h>
+int main()
+{
+    
+    return 0;
+}
---
+++ array/M_Replace_MinMax.c
@@ -0,0 +1,9 @@
+/*
+* url: https://codeforces.com/group/MWSDmqGsZm/contest/219774/problem/M */
+
+#include <stdio.h>
+int main()
+{
+    
+    return 0;
+}
